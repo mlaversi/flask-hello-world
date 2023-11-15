88ce386e8133225f0e28c806f1452fa5a33d93c8
Mathieu Laversin <mathieulaversin@outlook.fr>
2023-11-15 15:02:00 +0100

    dataframe send

--- app.py
+++ app.py
@@ -1,6 +1,50 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pandas as pd 
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'COUCOU, TRIPLE MONSTRE BROTHER'
+    return 'COUCOU, TRIPLE MONSTRE BROTHER' 
+
+# Création des données fictives
+data = {
+    'Nom': ['Alice', 'Bob', 'Charlie', 'David', 'Emma'],
+    'Âge': [25, 30, 22, 35, 28]
+}
+
+# Création du DataFrame
+df = pd.DataFrame(data)
+
+@app.route('/dataframe')
+def get_dataframe():
+    # Convertir le DataFrame en format JSON
+    dataframe_json = df.to_json(orient='records')
+
+    return jsonify({'dataframe': dataframe_json})
+
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+    if 'audio' not in request.files:
+        return 'Aucun fichier audio dans la requête', 400
+
+    audio_file = request.files['audio']
+
+    # Traitez le fichier audio ici selon vos besoins.
+    # Par exemple, vous pouvez le sauvegarder sur le serveur et effectuer des transformations.
+
+    # Simulons une transformation en ajoutant un préfixe au texte
+    transformed_audio = "Transformed: " + audio_file.read().decode('utf-8')
+
+    # Simulons des métriques
+    metrics = {'duration': 120, 'size': len(transformed_audio)}
+
+    # Retournez les données traitées à FlutterFlow
+    response_data = {'transformed_audio': transformed_audio, 'metrics': metrics}
+    
+    return jsonify(response_data), 200
+
+# For running the app locally
+if __name__ == '__main__':
+    app.run()
+    
